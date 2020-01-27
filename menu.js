6286f970963dad320df73e1b6fa2cb737a91041b
Aurelien <aurelien.belingard@gmail.com>
2020-01-27 14:44:31 +0100

    mise en page

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
 const menu = `
-<ul>
-    <li><a href="index.html">Accueil</a></li>
-    <li><a href="contact.html">Contact</a></li>
-    <li><a href="presentation.html">Presentation</a></li>
+<a class="navbar-brand" href="#">Git</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item"><a class="nav-link" href="index.html">Accueil</a></li>
+    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+    <li class="nav-item"><a class="nav-link" href="presentation.html">Presentation</a></li>
 </ul>
+</div>
 `;
 document.getElementById("navigation").innerHTML = menu;
